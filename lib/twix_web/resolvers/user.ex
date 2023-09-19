7be1076305fb6b596f780bb3a1960f1ce1beb2f6
Alex Nogueira <alex@devaction.com.br>
2023-09-19 08:03:08 -0300

    feat(app): simplify subscription
    simplify subscription with trigger

--- lib/twix_web/resolvers/user.ex
+++ lib/twix_web/resolvers/user.ex
@@ -5,9 +5,10 @@ defmodule TwixWeb.Resolvers.User do
   def create(%{input: params}, _context), do: Twix.create_user(params)
   def update(%{input: params}, _context), do: Twix.update_user(params)
   def add_follower(%{input: %{user_id: user_id, follower_id: follower_id}}, _context) do
-    with {:ok, result} <- Twix.add_follower(user_id, follower_id) do
-      Absinthe.Subscription.publish(TwixWeb.Endpoint, result, new_follower: "new_follow_topic")
-      {:ok, result}
-    end
+    Twix.add_follower(user_id, follower_id)
+#    with {:ok, result} <- Twix.add_follower(user_id, follower_id) do
+#      Absinthe.Subscription.publish(TwixWeb.Endpoint, result, new_follower: "new_follow_topic")
+#      {:ok, result}
+#    end
   end
 end
