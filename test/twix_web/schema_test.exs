bd6784db660f7e4b006720fa15931820db9590ae
Alex Nogueira <alex@devaction.com.br>
2023-12-03 18:19:22 -0300

    test(chore): Add new follow subscription test

--- test/twix_web/schema_test.exs
+++ test/twix_web/schema_test.exs
@@ -1,6 +1,6 @@
 defmodule SchemaTest do
   use ExUnit.Case
-  use TwixWeb.ConnCase
+  use TwixWeb.ConnCase, async: true
 
   describe "users query" do
     test "returns a user", %{conn: conn} do
@@ -93,6 +93,63 @@ defmodule SchemaTest do
       |> json_response(200)
 
       assert %{"data" => %{"createUser" => %{"age" => 20, "email" => "bruna@gmail.com.br", "id" => _id, "nickname" => "Bruna"}}} = response
+    end
+  end
+
+  describe "subscription" do
+    test "new follow", %{socket: socket} do
+      user_parmas = %{nickname: "Jose1", age: 19, email: "jose1@devaction.com"}
+
+      {:ok, user1} = Twix.create_user(user_parmas)
+
+      user_parmas = %{nickname: "Jose2", age: 33, email: "jose2@devaction.com"}
+
+      {:ok, user2} = Twix.create_user(user_parmas)
+
+      mutation = """
+        mutation{
+          addFollower(input: {userId: #{user1.id}, followerId: #{user2.id}}){
+            followerId
+            followingId
+          }
+        }
+      """
+
+      subscription = """
+        subscription{
+          newFollower{
+            followerId
+            followingId
+          }
+        }
+      """
+
+      #setup subscription
+      socket_ref = push_doc(socket, subscription)
+      assert_reply socket_ref, :ok, %{subscriptionId: subscription_id}
+
+      #setup mutation
+      socket_ref = push_doc(socket, mutation)
+      assert_reply socket_ref, :ok, mutation_response
+
+      #assert subscription
+      expected_response = %{
+        data: %{"addFollower" => %{"followerId" => "#{user2.id}", "followingId" => "#{user1.id}"}}
+      }
+      assert mutation_response == expected_response
+
+      #assert subscription
+      assert_push "subscription:data", subscription_response
+
+      expected_sub_response = %{
+        result: %{
+          data: %{
+            "newFollower" => %{
+              "followerId" => "#{user2.id}", "followingId" => "#{user1.id}"}}},
+        subscriptionId: "#{subscription_id}"
+      }
+
+      assert subscription_response == expected_sub_response
     end
   end
 end
